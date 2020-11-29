9804f4b0238506361844ba665a8a0797cd6ea6ae
oksil joo <oksiljoo@oksilui-MacBook-Pro.local>
2020-11-28 20:53:16 -0500

    addWebproject

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+//pickle jar
+var firstName = " Oksil";
+
+//fire alert, add string, concatenate, dump in varible
+//The alert() method displays an alert box
+alert("Hello"+firstName);
+
+alert('Hello ${firstName}!!!');
+
+console.log(firstName);
